356d21f6d10a89bd273dce47932bcaa85c76f1ea
Aleksei Gusev <aleksei.gusev@gmail.com>
2022-12-06 19:33:12 +0200

    Add Day 3, part 1

---
+++ src/bin/day03p01.rs
@@ -0,0 +1,6 @@
+use advent_of_code_2022::day_03;
+
+fn main() {
+    let puzzle_input = advent_of_code_2022::get_puzzle_input();
+    println!("Total score {}", day_03::part_01(&puzzle_input));
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::fs;
 
 pub mod day_01;
 pub mod day_02;
+pub mod day_03;
 
 pub fn get_puzzle_input() -> String {
     let puzzle_input_path = env::args()
